e906dcd3dfe1d5cde9f25a88f041ec9faa90a282
rich5k <richardanatsui@gmail.com>
2021-04-06 20:53:51 +0100

    created a file for database credentials

---
+++ controller/database.php
@@ -0,0 +1,12 @@
+<?php
+
+
+
+//DB Params
+define("DB_HOST","127.0.0.1");
+define("DB_USER","root");
+
+define("DB_PASS", "");
+// define("DB_PASS",getenv(MYSQLPASS) != null ? getenv(MYSQLPASS): "");
+define("DB_NAME","sAttendance");
+?>
