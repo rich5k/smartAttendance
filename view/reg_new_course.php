3cf7f6215650f71d84bd7cc4dc2685354382661e
rich5k <richardanatsui@gmail.com>
2021-04-07 01:04:54 +0100

    completed basic layout for registry create course and lecturer start class

--- view/reg_new_course.php
+++ view/reg_new_course.php
@@ -1,12 +1,93 @@
+<?php
+require_once '../controller/database.php';
+require_once '../models/Student.php';
+require_once '../models/Database.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Create Course</title>
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/sdashboard.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
+
 <body>
     
+	<!-- navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top" style="background-color: #264C69;">
+		<div class="container">
+			<a class="navbar-brand" href="../index.php"><img src="../assets/logo1.jpg" alt=""></a>
+			<!-- Hamburger -->
+		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+		    <span class="navbar-toggler-icon"></span>
+		  </button>
+		  <!-- Collapses navbar items into Hamburger -->
+		  <div class="collapse navbar-collapse" id="navbarNav">
+		    
+			<ul class="navbar-nav my-2 my-lg-0 ml-auto">
+                <li class="nav-item active" >
+		            <a class="nav-link" href="student_dashboard.php">Dashboard</a>
+		        </li>
+                <li class="nav-item " >
+		            <a class="nav-link" href="#">My Courses</a>
+		        </li>
+                <li class="nav-item" >
+		            <a class="nav-link" href="#">Timetable</a>
+		        </li>
+                
+                <li class="nav-item" >
+		            <a class="nav-link" href="settings.php"><i class="fa fa-cog" aria-hidden="true"></i></a>
+		        </li>
+		      
+
+			  
+			</ul>
+			</div>
+
+ 		</div>
+    </nav>
+    <br>
+    
+	
+	<!-- Dashboard -->
+   <div class="container jumbotron course">
+        <h1>Create new Course</h1>
+        <form>
+        <div class="form-group">
+            <label for="cCode">Course Code</label>
+            <input type="text" class="form-control" id="cCode">
+            
+        </div>
+        <div class="form-group">
+            <label for="cName">Course Name</label>
+            <input type="text" class="form-control" id="cName">
+            
+        </div>
+        <div class="form-group">
+            <label for="faculty">Faculty</label>
+            <input type="text" class="form-control" id="faculty">
+            
+        </div>
+        <div class="form-group">
+            <label for="students">Students</label>
+            <input type="text" class="form-control" id="students" ">
+            
+        </div>
+        <div class="form-group">
+            <label for="cschedule">Class Schedule</label>
+            <input type="text" class="form-control" id="cschedule" >
+            
+        </div>
+        
+        <button type="submit" class="btn btn-primary">Create</button>
+        </form>
+        
+   </div>
+	
+	<script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
+	<script type="text/javascript" src="../js/bootstrap.min.js"></script>
 </body>
 </html>
