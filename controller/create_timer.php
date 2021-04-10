d1373ca1576d9a98bfb867dce1290dcc1b53ad06
rich5k <richardanatsui@gmail.com>
2021-04-10 23:50:57 +0100

    modified backend file for class timer

--- controller/create_timer.php
+++ controller/create_timer.php
@@ -21,28 +21,145 @@ if(isset($_POST['submit'])){
    
     
     else{
-        
-        //classTimer Data
-        $classTimerData=[
-            'lecturer'=>$lectID,
-            'courseID'=>$courseID,
-            'duration'=>$duration,
-            'checknum'=>$checks
-        ];
-
-        
-        //Add lecturer To Do
-        if($lecturer->addClassTimer($classTimerData)){
-            echo '<script>alert("Well Done. You added a lecturer to the course successfully")</script>';
-            echo '<script>window.location.href = "../view/lecturer_cclass.php";</script>';
-            exit();
-
+        $realDuration=0.0;
+        if ($duration=="30 mins"){
+            $realDuration=0.5;
+            //classTimer Data
+            $classTimerData=[
+                'lecturer'=>$lectID,
+                'courseID'=>$courseID,
+                'duration'=>$realDuration,
+                'checknum'=>$checks
+            ];
+    
+            
+            //Add class timer To Do
+            if($lecturer->addClassTimer($classTimerData)){
+                echo '<script>alert("Well Done. You have set a timer for the course successfully")</script>';
+                echo '<script>window.location.href = "../view/lecturer_cclass.php";</script>';
+                exit();
+    
+            }
+            else{
+                header("Location: ../view/lecturer_sclass.php?error=sqlerror1");
+                exit();
+            }
+        }
+        elseif ($duration=="1 hour"){
+            $realDuration=1.0;
+            //classTimer Data
+            $classTimerData=[
+                'lecturer'=>$lectID,
+                'courseID'=>$courseID,
+                'duration'=>$realDuration,
+                'checknum'=>$checks
+            ];
+    
+            
+            //Add class timer To Do
+            if($lecturer->addClassTimer($classTimerData)){
+                echo '<script>alert("Well Done. You have set a timer for the course successfully")</script>';
+                echo '<script>window.location.href = "../view/lecturer_cclass.php";</script>';
+                exit();
+    
+            }
+            else{
+                header("Location: ../view/lecturer_sclass.php?error=sqlerror1");
+                exit();
+            }
+        }
+        elseif ($duration=="1 hour 30 mins"){
+            $realDuration=1.5;
+            //classTimer Data
+            $classTimerData=[
+                'lecturer'=>$lectID,
+                'courseID'=>$courseID,
+                'duration'=>$realDuration,
+                'checknum'=>$checks
+            ];
+    
+            
+            //Add class timer To Do
+            if($lecturer->addClassTimer($classTimerData)){
+                echo '<script>alert("Well Done. You have set a timer for the course successfully")</script>';
+                echo '<script>window.location.href = "../view/lecturer_cclass.php";</script>';
+                exit();
+    
+            }
+            else{
+                header("Location: ../view/lecturer_sclass.php?error=sqlerror1");
+                exit();
+            }
+        }
+        elseif ($duration=="2 hour"){
+            $realDuration=2.0;
+            //classTimer Data
+            $classTimerData=[
+                'lecturer'=>$lectID,
+                'courseID'=>$courseID,
+                'duration'=>$realDuration,
+                'checknum'=>$checks
+            ];
+    
+            
+            //Add class timer To Do
+            if($lecturer->addClassTimer($classTimerData)){
+                echo '<script>alert("Well Done. You have set a timer for the course successfully")</script>';
+                echo '<script>window.location.href = "../view/lecturer_cclass.php";</script>';
+                exit();
+    
+            }
+            else{
+                header("Location: ../view/lecturer_sclass.php?error=sqlerror1");
+                exit();
+            }
+        }
+        elseif ($duration=="2 hour 30 mins"){
+            $realDuration=2.5;
+            //classTimer Data
+            $classTimerData=[
+                'lecturer'=>$lectID,
+                'courseID'=>$courseID,
+                'duration'=>$realDuration,
+                'checknum'=>$checks
+            ];
+    
+            
+            //Add class timer To Do
+            if($lecturer->addClassTimer($classTimerData)){
+                echo '<script>alert("Well Done. You have set a timer for the course successfully")</script>';
+                echo '<script>window.location.href = "../view/lecturer_cclass.php";</script>';
+                exit();
+    
+            }
+            else{
+                header("Location: ../view/lecturer_sclass.php?error=sqlerror1");
+                exit();
+            }
         }
         else{
-            header("Location: ../view/lecturer_sclass.php?error=sqlerror1");
-            exit();
+            $realDuration=3.0;
+            //classTimer Data
+            $classTimerData=[
+                'lecturer'=>$lectID,
+                'courseID'=>$courseID,
+                'duration'=>$realDuration,
+                'checknum'=>$checks
+            ];
+    
+            
+            //Add class timer To Do
+            if($lecturer->addClassTimer($classTimerData)){
+                echo '<script>alert("Well Done. You have set a timer for the course successfully")</script>';
+                echo '<script>window.location.href = "../view/lecturer_cclass.php";</script>';
+                exit();
+    
+            }
+            else{
+                header("Location: ../view/lecturer_sclass.php?error=sqlerror1");
+                exit();
+            }
         }
-        
     }
 }
 
